02e192ebcff0412f50a35ae67fe67211746e4e69
Joshua Wootonn <joshuawootonn@gmail.com>
2022-12-29 13:21:46 -0600

    feat: rewrite single selection up to node collapsing

--- components/single-selection-article/tree-node.tsx
+++ components/single-selection-article/tree-node.tsx
@@ -1,57 +1,29 @@
 import classNames from 'classnames'
-import isHotkey from 'is-hotkey'
 import { TreeNodeType } from 'lib/common/types'
-import {
-  TreeActionTypes,
-  TreeViewContext,
-} from 'lib/single-selection-article/tree-context'
-import {
-  getNextNode,
-  getPreviousNode,
-} from 'lib/single-selection-article/tree-traversal'
-import { useContext } from 'react'
+import { useTreeNode } from 'lib/single-selection-article/useTreeNode'
 
 type TreeNodeProps = {
   node: TreeNodeType
 }
 
 export function TreeNode({ node }: TreeNodeProps) {
-  const { state, dispatch, elements } = useContext(TreeViewContext)
+  const { isSelected, treeNodeProps } = useTreeNode(node.id)
   return (
     <li
       className={classNames(
-        state.selectedId === node.id
-          ? 'bg-slate-200'
-          : 'bg-transparent',
+        'focus:outline-none [&:focus>div]:border-slate-400',
       )}
-      ref={element => {
-        if (element) {
-          elements.current.set(node.id, element)
-        } else {
-          elements.current.delete(node.id)
-        }
-      }}
-      tabIndex={state.selectedId === node.id ? 0 : -1}
-      onClick={e => {
-        e.stopPropagation()
-        dispatch({ type: TreeActionTypes.SELECT, id: node.id })
-      }}
-      onKeyDown={e => {
-        e.stopPropagation()
-        console.log({ onKeyDown: 'true' })
-
-        if (isHotkey('down', e)) {
-          const nextNode = getNextNode(state, node.id)
-          elements.current.get(nextNode)?.focus()
-        }
-        if (isHotkey('up', e)) {
-          const prevNode = getPreviousNode(state, node.id)
-          elements.current.get(prevNode)?.focus()
-        }
-      }}
+      {...treeNodeProps}
     >
-      {node.name}
-      <ul className="pl-4">
+      <div
+        className={classNames(
+          'border-[1.5px] border-transparent',
+          isSelected ? 'bg-slate-200' : 'bg-transparent',
+        )}
+      >
+        {node.name}
+      </div>
+      <ul className="pl-4 ">
         {node.children?.map(node => (
           <TreeNode key={node.id} node={node} />
         ))}
